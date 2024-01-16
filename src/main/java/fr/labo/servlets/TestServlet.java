eb048e66fb99aab90609a6539b23753494dbe7c2
OGENDT <olivia_gendt@outlook.fr>
2024-01-16 16:31:52 +0100

    Creation Package Servlets + Supression Package Test

--- src/main/java/fr/labo/servlets/TestServlet.java
+++ src/main/java/fr/labo/servlets/TestServlet.java
@@ -1,24 +1,23 @@
-package pk.pour.test;
+package fr.labo.servlets;
 
 import java.io.IOException;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletTest
+ * Servlet implementation class TestServlet
  */
-@WebServlet("/ServletTest")
-public class ServletTest extends HttpServlet {
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /** 
+    /**
      * Default constructor. 
      */
-    public ServletTest() {
+    public TestServlet() {
         // TODO Auto-generated constructor stub
     }
 
