cf722e020d1c77ff4112cbb942c3b67ccce0a9bf
benJ78405 <156656566+benJ78405@users.noreply.github.com>
2024-01-17 16:45:36 +0100

    Revert "Ajout servlet-jsp connexion user"
    This reverts commit cc3438eb79026d6ab1f8c88dcc0480d4bf66e313.

--- src/main/java/fr/labo/servlets/TestServlet.java
+++ src/main/java/fr/labo/servlets/TestServlet.java
@@ -1,24 +1,32 @@
 package fr.labo.servlets;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ServletAccesConnectionUserJsp")
-public class ServletAccesConnectionUserJsp extends HttpServlet {
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/connectionUser.jsp");
-		rd.forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
