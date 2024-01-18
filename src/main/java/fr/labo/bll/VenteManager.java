c8e3ab57d7a23fa4e40124c9bb6b132bb70a24d0
Alin1233 <herciualin10@gmail.com>
2024-01-18 16:24:31 +0100

    Methodes dans Vente Manager

--- src/main/java/fr/labo/bll/VenteManager.java
+++ src/main/java/fr/labo/bll/VenteManager.java
@@ -1,12 +1,26 @@
 package fr.labo.bll;
 
+import java.util.List;
+
 import fr.labo.bo.ArticleVendu;
+import fr.labo.bo.Categorie;
 import fr.labo.dal.vente.VenteDAO;
 
 public class VenteManager {
 	
 	VenteDAO venteDAO;
+	
+	//Rien ne fonctionne ici pour l'instant!!!!!!!!
+	
 	public void ajuterVente(ArticleVendu article) {
 		venteDAO.insert(article);
+	}
+	
+	public List<ArticleVendu> getAll(){
+		return venteDAO.selectAll();
+	}
+	
+	public List<ArticleVendu> getCategorie(Categorie categorie){
+		return venteDAO.selectCategorie(categorie);
 	}
 }
--- src/main/java/fr/labo/dal/vente/VenteDAOJdbcImpl.java
+++ src/main/java/fr/labo/dal/vente/VenteDAOJdbcImpl.java
@@ -1,6 +1,9 @@
 package fr.labo.dal.vente;
 
+import java.util.List;
+
 import fr.labo.bo.ArticleVendu;
+import fr.labo.bo.Categorie;
 
 public class VenteDAOJdbcImpl implements VenteDAO {
 
@@ -8,6 +11,18 @@ public class VenteDAOJdbcImpl implements VenteDAO {
 	public void insert(ArticleVendu vente) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public List<ArticleVendu> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ArticleVendu> selectCategorie(Categorie categorie) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
