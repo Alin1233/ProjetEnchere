8b8022160dbfa74b5ea48b6ee4231248d8b93ed0
Alin1233 <herciualin10@gmail.com>
2024-01-18 15:31:49 +0100

    Create ArticleVendu.java

---
+++ src/main/java/fr/labo/bo/ArticleVendu.java
@@ -0,0 +1,17 @@
+package fr.labo.bo;
+
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private String dateDebutEncheres;
+	private String dateFinEncheres;
+	private Float miseAPrix;
+	private Float prixVente;
+	private String etatVente;
+	
+	private Categorie categorie;
+	private Utilisateur vendeur;
+	private Adresse retrait;
+	
+}
