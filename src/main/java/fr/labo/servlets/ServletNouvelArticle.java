d8fe7f4aebcb421b65f35ed2e8d6057051c0266a
Louis-Philippe86 <louis.philippe.coronas@gmail.fr>
2024-01-25 09:23:27 +0100

    modif
    request.getRequestDispatcher("nouvelleVente.jsp").forward(request, response);

--- src/main/java/fr/labo/servlets/ServletNouvelArticle.java
+++ src/main/java/fr/labo/servlets/ServletNouvelArticle.java
@@ -74,6 +74,7 @@ public class ServletNouvelArticle extends HttpServlet {
 		
 		// Ajouter un attribut à la requête pour indiquer la réussite
 		request.setAttribute("confirmationMessage", "L'article a été ajouté avec succès.");
+		request.getRequestDispatcher("nouvelleVente.jsp").forward(request, response);
 		
 		//Erreur, format différent attendue par la base de donnée
 	   }catch (SQLException e) {
